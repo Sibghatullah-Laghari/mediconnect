d4683705a103992e3e6f482873bbda72693d56b6
sibghatullah <siblaghari@gmail.com>
2026-06-01 14:06:15 +0500

    Fixes in DTOs

---
+++ backend/src/main/java/com/mediconnect/dto/appointment/AppointmentResponse.java
@@ -0,0 +1,29 @@
+package com.mediconnect.dto.appointment;
+
+import com.mediconnect.model.AppointmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+
+public class AppointmentResponse {
+
+    private Long id;
+    private AppointmentStatus status;
+
+    private String reason;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private Long patientId;
+    private Long doctorId;
+    private String patientName;
+    private String doctorName;
+}
