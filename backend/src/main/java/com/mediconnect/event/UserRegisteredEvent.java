676b5528672d0907ef01e62504f640009f09ceea
Sibghatullah Laghari <Siblaghari@gmail.com>
2026-07-14 21:11:58 +0500

    Update UserRegisteredEvent.java

--- backend/src/main/java/com/mediconnect/event/UserRegisteredEvent.java
+++ backend/src/main/java/com/mediconnect/event/UserRegisteredEvent.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationEvent;
 @Getter
 public class UserRegisteredEvent extends ApplicationEvent {
 
+    // User associated with the registration event
     private final User user;
 
     public UserRegisteredEvent(User user) {
