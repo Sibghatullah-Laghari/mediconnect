f98aef371e74c528e06eae36ef102a88b23860f1
Sibghatullah Laghari <Siblaghari@gmail.com>
2026-08-28 21:41:02 +0500

    Fix comment formatting in StatusRequest.java
    Corrected comments in StatusRequest.java to remove extra periods.

--- backend/src/main/java/com/mediconnect/dto/appointment/StatusRequest.java
+++ backend/src/main/java/com/mediconnect/dto/appointment/StatusRequest.java
@@ -4,9 +4,9 @@ import com.mediconnect.model.AppointmentStatus;
 import jakarta.validation.constraints.NotNull;
 
 /**
- * Request DTO used to update the status of an appointment.
+ * Request DTO used to update the status of an appointment..
  *
- * @param status the new appointment status
+ * @param status the new appointment status..
  */
 public record StatusRequest(
 
